64f65f4e461c7bfad6678d2134bdb493adfccfaa
Aistegitas <ajjstee@gmail.com>
2026-01-22 21:56:47 +0200

    korteliu skaiciavimas, spalvinimas

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const carForm = document.getElementById("carForm");
 const carsDiv = document.getElementById("cars");
+const carCount = document.getElementById("carCount");
+let carCounter = 0;
 
 carForm.addEventListener("submit", function (event) {
   // Sustabdys standartine formos elgsena, nepekraus puslapio
@@ -13,9 +15,27 @@ carForm.addEventListener("submit", function (event) {
   const image = document.getElementById("image").value;
   const fuel = document.getElementById("fuel").value;
 
+  if (name.length < 3) {
+    alert("Pavadinimas turi buti bent 3 simboliu");
+    return;
+  }
+
+  if (price <= 0) {
+    alert("Kaina turi buti didesne uz 0");
+    return;
+  }
+
   // sukuriu automobilio kortele
   const carCard = document.createElement("div");
   carCard.className = "car-card";
+
+  if (fuel === "Benzinas") {
+    carCard.classList.add("benzinas");
+  } else if (fuel === "Dyzelinas") {
+    carCard.classList.add("dyzelinas");
+  } else if (fuel === "Elektra") {
+    carCard.classList.add("elektra");
+  }
 
   // pripildau kortele informacija
   carCard.innerHTML = `
@@ -33,10 +53,14 @@ carForm.addEventListener("submit", function (event) {
   const deleteBtn = carCard.querySelector(".delete-btn");
   deleteBtn.addEventListener("click", () => {
     carCard.remove();
+    carCounter--;
+    carCount.textContent = `Automobiliu skaicius: ${carCounter}`;
   });
 
   // pridedu kortele i HTML'a
   carsDiv.appendChild(carCard);
+  carCounter++;
+  carCount.textContent = `Automobiliu skaicius: ${carCounter}`;
 
   // Atsatyk forma i pradine padeti
   carForm.reset();
